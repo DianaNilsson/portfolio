a8e997ecf66be4dae022162ba1de60114e047b86
Diana <diana.sofie.nilsson@gmail.com>
2021-05-28 09:03:35 +0200

    Reset css

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/main.scss'],
+  css: ['@/assets/main.scss', '@/assets/reset.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
