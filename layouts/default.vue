9a153b3341e91eda17f608fc1e576f4e47686e09
Diana <diana.sofie.nilsson@gmail.com>
2021-05-28 08:20:36 +0200

    Configure sass

--- layouts/default.vue
+++ layouts/default.vue
@@ -49,26 +49,6 @@ export default {
 </script>
 
 <style>
-/* Reset */
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
 .bg-image {
   position: absolute;
   top: 0;
@@ -140,7 +120,6 @@ a {
 .collapse {
   background-color: #a4484e;
   border-radius: 5px;
-  border: none;
   padding: 0.5rem 1rem;
 
   position: absolute;
