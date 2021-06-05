451eb6270fc4c969fa18d5261922047264f0e221
Diana <diana.sofie.nilsson@gmail.com>
2021-06-05 09:07:55 +0200

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,33 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
     node: true,
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
   },
   extends: [
     '@nuxtjs',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
   plugins: [],
+  root: true,
   // add your custom rules here
-  rules: {},
+  rules: {
+    'no-console': 'off',
+    semi: [2, 'never'],
+    'sort-keys': ['error', 'asc'],
+    'vue/attributes-order': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          component: 'always',
+          normal: 'always',
+          void: 'always',
+        },
+      },
+    ],
+  },
 }
