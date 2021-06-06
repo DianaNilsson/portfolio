59c6d890cc7929ebc8163321329020c17bddf1e0
Diana <diana.sofie.nilsson@gmail.com>
2021-06-06 18:40:08 +0200

    Fetch content

---
+++ pages/projekt/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <h1>{{ project.title }}</h1>
+    <nuxt-content :document="project" />
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const project = await $content('projects', params.slug).fetch()
+
+    return {
+      project,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
